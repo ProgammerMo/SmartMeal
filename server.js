9277aa74735b1f42eba7c800421574a5d06d58fd
Mohamed <programmer_mo@icloud.com>
2021-06-17 01:13:43 +0200

    Fixing db

--- server.js
+++ server.js
@@ -7,12 +7,9 @@ const search = require('./controllers/search');
 const api = require('./controllers/api');
 const db = require('knex')({
     client: 'pg',
-    connection: {
-        host : 'ec2-52-19-170-215.eu-west-1.compute.amazonaws.com',
-        user : 'pfhvdmsbtmpvoo',
-        password : '352601895fa54d9b30d01ab877f8455df80710a3166b926ff95a2dc87cd604dc',
-        database : 'd4j8aqedlhhvuj',
-        uri: 'postgres://pfhvdmsbtmpvoo:352601895fa54d9b30d01ab877f8455df80710a3166b926ff95a2dc87cd604dc@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d4j8aqedlhhvuj'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
     }
 });
 
