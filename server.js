68e7f8faf91286887118611234fe5887bf49c0f4
Mohamed <programmer_mo@icloud.com>
2021-06-17 00:21:05 +0200

    Fixing db

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const db = require('knex')({
     client: 'pg',
     connection: {
         connectionString : process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+          }
     }
 });
 
