b26349af5494cc3fcb4b2046b09a9ff10d004171
Mohamed <programmer_mo@icloud.com>
2021-06-17 01:19:22 +0200

    Fixing db

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const search = require('./controllers/search');
 const api = require('./controllers/api');
 const db = require('knex')({
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+        directory: './db/migrations'
+    },
+    useNullAsDefault: true
 });
 
 const app = express();
