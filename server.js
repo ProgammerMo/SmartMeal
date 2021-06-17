fd4c27bbaedf67af58d82f7904ea0316ea63c1de
Mohamed <programmer_mo@icloud.com>
2021-06-17 14:00:40 +0200

    Fixing db

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const api = require('./controllers/api');
 const db = require('knex')({
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl : true,
     pool: {
         min: 2,
         max: 10
